1561360f6b92c2fe335d930b1a16158de76e9692
Bullets-jyr <jyr@bullets.co.kr>
2023-07-24 08:51:04 +0900

    05. 화면 구성하기 1

--- app/src/main/java/kr/co/bullets/part3chapter6/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part3chapter6/MainActivity.kt
@@ -3,11 +3,16 @@ package kr.co.bullets.part3chapter6
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
+import kr.co.bullets.part3chapter6.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
     }
 }
