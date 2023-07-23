99f45ebf5e093f11da22cfa0cccc4f1b2301b00e
Bullets-jyr <jyr@bullets.co.kr>
2023-07-23 20:30:25 +0900

    04. Hilt 적용하기

---
+++ app/src/main/java/kr/co/bullets/part3chapter6/HiltApplication.kt
@@ -0,0 +1,8 @@
+package kr.co.bullets.part3chapter6
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application() {
+}
--- app/src/main/java/kr/co/bullets/part3chapter6/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part3chapter6/MainActivity.kt
@@ -2,7 +2,9 @@ package kr.co.bullets.part3chapter6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
